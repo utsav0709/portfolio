533cd1d7e3eae27dce93476f89f4570275dd71cf
Utsav Gupta <utsavgupta@Utsavs-MacBook-Air.local>
2018-10-24 11:55:39 +0530

    initial build

---
+++ resources/js/helper.js
@@ -0,0 +1,64 @@
+// Animation Helper
+function promiseAnimate ({duration, timing, draw}) {
+  return new Promise (function (resolve) {
+    function animate ({duration, timing, draw}) {
+      const start = performance.now();
+
+      requestAnimationFrame(function animate(time) {
+        var timeFraction = (time - start) / duration;
+        if (timeFraction > 1) {
+          timeFraction = 1;
+          resolve(duration)
+        }
+        const progress = timing(timeFraction)
+
+        draw(progress);
+        if (timeFraction < 1) {
+          requestAnimationFrame(animate)
+        }
+      })
+    }
+    animate({duration, timing, draw})
+  })
+}
+// timing functions
+
+function bounce(timeFraction) {
+      for (var a = 0, b = 1, result; 1; a += b, b /= 2) {
+        if (timeFraction >= (7 - 4 * a) / 11) {
+          return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+        }
+      }
+    }
+    function circ (timeFraction) {
+      return 1 - Math.sin(Math.acos(timeFraction))
+    }
+
+    function linear (timeFraction) {
+      return timeFraction
+    }
+    // getting coordinates of section
+    function getTopCoordinates (elem) {
+      return elem.getBoundingClientRect().top + pageYOffset;
+    }
+
+    // element visibility helper functions
+
+    function isPartiallyVisible(el) {
+          var elementBoundary = el.getBoundingClientRect();
+
+          var top = elementBoundary.top;
+          var bottom = elementBoundary.bottom;
+          var height = elementBoundary.height;
+
+          return ((top + height >= 0) && (height + window.innerHeight >= bottom));
+        }
+
+        function isFullyVisible(el) {
+          var elementBoundary = el.getBoundingClientRect();
+
+          var top = elementBoundary.top;
+          var bottom = elementBoundary.bottom;
+
+          return ((top >= 0) && (bottom <= window.innerHeight));
+        }
---
+++ resources/js/hero.js
@@ -0,0 +1,147 @@
+
+// getting the hero text
+const hero = document.querySelector('.hero-container--desktop')
+const heroText = document.getElementById('heroText').firstElementChild;
+const welcomeImg = document.getElementById('welcomeImg');
+const textElem = document.querySelectorAll('.text-elem')
+console.log(textElem);
+    // initial animation sequence
+    function initialAnimationSequence(textArea) {
+      const text = textArea.innerText;
+      const to = text.length,
+        from = 0;
+
+        promiseAnimate({
+          duration: 1000,
+          timing: linear,
+          draw: function(progress) {
+            hero.style.opacity = progress;
+          }
+        }).then(function(result) {
+          return new Promise(function(resolve) {
+            textElem[0].classList.add('animated')
+            textElem[0].classList.add('fadeInUp')
+            resolve(1);
+          })
+        }).then(function(result) {
+          return new Promise(function(resolve) {
+            setTimeout(function() {
+              textElem[1].classList.add('animated')
+              textElem[1].classList.add('fadeInUp')
+              resolve(1);
+            }, 1000)
+          })
+        })
+        promiseAnimate({
+          duration: 9000,
+          timing: bounce,
+          draw: function(progress) {
+            const result = (to - from) * progress + from;
+            textArea.innerHTML = text.substr(0, Math.ceil(result))
+          }
+        }).then(function (result) {
+          promiseAnimate({
+            duration: 1000,
+            timing: circ,
+            draw: function (progress) {
+              heroText.style.width = (100 - 50 * progress) + '%';
+            }
+          }).then(function(result) {
+            return new Promise(function(resolve) {
+              console.log('s4 start');
+              hero.classList.add('hinge');
+              resolve(1);
+              })
+          }).then(function(result) {
+            return new Promise(function(resolve) {
+              setTimeout(() => {
+                console.log('s5 start');
+                promiseAnimate({
+                  duration: 900,
+                  timing: linear,
+                  draw: function(progress) {
+                    let currentScroll = window.pageYOffset;
+                    let finalScroll = currentScroll + (topCoord[0] - currentScroll) * progress;
+                    window.scrollTo(0, finalScroll);
+                  }
+                })
+                resolve(1)
+              }, 1200)
+            })
+          }).then(function(result) {
+            return new Promise(function(resolve) {
+              console.log('s6 start');
+              setTimeout(() => {
+                document.body.style.overflow = 'scroll';
+                  resolve(1)
+              }, 700)
+            })
+          })
+    })
+}
+
+
+    // calling the text animateText
+
+    document.addEventListener('DOMContentLoaded', function () {
+      initialAnimationSequence(heroText)
+    })
+    // section array
+
+    const nav = document.querySelector('.nav-container--mobile');
+    const exp = document.querySelector('.exp-container--desktop');
+    const work = document.querySelector('.work-container--desktop');
+    const about = document.querySelector('.me-container--desktop');
+    const contact = document.querySelector('.contact-container--desktop');
+    const navLinks = document.querySelectorAll('.nav-links--mobile span')
+
+
+    const sectionArr = [nav, work, exp, about, contact];
+
+    const topCoord = sectionArr.map(function (elem) {
+      return getTopCoordinates(elem);
+    })
+
+    // event listener for navLinks
+    navLinks.forEach(function (node, i) {
+      node.addEventListener('click', function() {
+        promiseAnimate({
+          duration: 600,
+          timing: linear,
+          draw: function(progress) {
+            let currentScroll = window.pageYOffset;
+            let finalScroll = currentScroll + (topCoord[i + 1] - currentScroll) * progress;
+            window.scrollTo(0, finalScroll);
+          }
+        })
+      })
+    })
+
+    // scroll reveal
+
+    const cardArr = document.querySelectorAll('.card-reveal')
+    console.log(cardArr);
+    let isScrolling = false;
+    function throttleScroll(e) {
+      if (isScrolling == false) {
+        window.requestAnimationFrame(function() {
+          scrolling(e);
+          isScrolling = false;
+        });
+      }
+      isScrolling = true;
+    }
+    function scrolling(e) {
+      cardArr.forEach(function(card) {
+        if(isPartiallyVisible(card)) {
+          card.classList.add('animated')
+          card.classList.add('bounceIn')
+        } else {
+          card.classList.remove('animated')
+          card.classList.remove('bounceIn')
+        }
+      })
+    }
+    window.addEventListener('scroll', () => {
+      throttleScroll()
+    })
